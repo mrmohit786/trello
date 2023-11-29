5c24345e38afe3c53656e53a06ad5e69bb33664d
Mohit Patel <patelmohit719@gmail.com>
2023-11-29 20:08:26 +0530

    Finished Marketing Page and authorization setup with Clerk

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,7 @@
+const ClerkLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full items-center justify-center flex">{children}</div>
+  );
+};
+
+export default ClerkLayout;
